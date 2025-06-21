c8c19c8017ec07b5b39b49f99416d592f82390e6
JeetDas5 <jeet15083011@gmail.com>
2025-06-21 11:06:07 +0530

    feat: implement zod validation

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,7 +2,11 @@ import * as schema from "./schema";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE URL is not defined in the environment variables.");
+}
+
+const sql = neon(process.env.DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
 
